e74be78973cf10595b7cd4dc5b5d9c8db8269043
Amin <asabe041@uottawa.ca>
2020-06-18 16:46:31 -0400

    final version of lab5

--- SEG3525-Lab5/scripts/calculate.js
+++ SEG3525-Lab5/scripts/calculate.js
@@ -1,7 +1,6 @@
 function addToCart(service, cart) {
-
     var s1 = document.getElementById(cart);
-    
+    s1.innerHTML="";
     var servicePriceId;
     servicePriceId = service+"-price";
     var s2 = document.getElementById(servicePriceId)
@@ -17,6 +16,26 @@ function addToCart(service, cart) {
     p.innerHTML = service;
     p.appendChild(s);
     s1.appendChild(p);
-  
-
 }
+
+//code inspired from https://stackoverflow.com/questions/15757918/disable-past-dates-on-datepicker
+
+  function changeDates(mechanic){
+    $( "#datepicker" ).datepicker({
+        minDate: 0,
+        beforeShowDay: function(date){
+            if(mechanic == "Erwind"){
+                var day = date.getDay();
+                return [(day != 4 && day != 5 && day != 6 && day != 0)];
+            }
+            if(mechanic == "Jane"){
+                var day = date.getDay();
+                return [(day != 6 && day != 0)];
+            }
+            if(mechanic == "Albert"){
+                var day = date.getDay();
+                return [(day != 1 && day != 5 && day != 6 && day != 0)];
+            }
+        }
+       });
+  }
